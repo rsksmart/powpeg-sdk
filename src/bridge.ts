155563a31d76bab98c98300df25bfd2d1ffd6f20
lserra-iov <lucio.serra@iovlabs.org>
2025-03-13 18:30:21 -0300

    Add pegout methods and consume ethers from core sdk
    Signed-off-by: lserra-iov <lucio.serra@iovlabs.org>

--- src/bridge.ts
+++ src/bridge.ts
@@ -1,9 +1,10 @@
 import { bridge } from '@rsksmart/rsk-precompiled-abis'
-import { ethers } from 'ethers'
+import { ethers } from '@rsksmart/bridges-core-sdk'
 import type { Network } from './constants'
 
 export class Bridge {
-  provider: ethers.Provider
+  address = '0x0000000000000000000000000000000001000006'
+  provider: ethers.providers.Provider
   bridgeContract: ethers.Contract
   publicNodes: Record<Network, string> = {
     MAIN: 'https://public-node.rsk.co',
@@ -11,11 +12,19 @@ export class Bridge {
   }
 
   constructor(network: Network, rpcProviderUrl?: string) {
-    this.provider = new ethers.JsonRpcProvider(rpcProviderUrl ?? this.publicNodes[network])
+    this.provider = new ethers.providers.JsonRpcProvider(rpcProviderUrl ?? this.publicNodes[network])
     this.bridgeContract = new ethers.Contract(bridge.address, bridge.abi, this.provider)
   }
 
   async getFederationAddress(): Promise<string> {
     return this.bridgeContract.getFederationAddress?.()
+  }
+
+  async getPegoutEstimatedFee(): Promise<bigint> {
+    const [nextPegoutCost, pegoutQueueCount] = await Promise.all<[ethers.BigNumber, ethers.BigNumber]>([
+      this.bridgeContract.getEstimatedFeesForNextPegOutEvent(),
+      this.bridgeContract.getQueuedPegoutsCount(),
+    ])
+    return nextPegoutCost.div(pegoutQueueCount.add(1n)).toBigInt()
   }
 }
