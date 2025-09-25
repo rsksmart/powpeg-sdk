3775731020e2f58242ca5ea42658d85d9221931b
lserra-iov <lucio.serra@iovlabs.org>
2025-09-25 17:08:20 -0300

    Use 2WP API v3.1.0

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,6 +3,19 @@ import { TxType } from '../types'
 import type { BitcoinDataSource, FeeLevel, Utxo, AddressWithDetails, StatusData } from '../types'
 import { type Network } from '../constants'
 import { APIError } from '../errors'
+import { ethers } from '@rsksmart/bridges-core-sdk'
+
+type UtxoResponse2WP = {
+  data: {
+    address: string
+    txid: string
+    vout: number
+    amount: string
+    satoshis: number
+    height: number
+    confirmations: number
+  }[]
+}
 
 export class ApiService implements BitcoinDataSource {
   private apiUrls: Record<Network, string> = {
@@ -40,27 +53,40 @@ export class ApiService implements BitcoinDataSource {
   async getFeeRate(level: FeeLevel): Promise<number> {
     const blocks = this.feeLevelBlocks[level]
     const response = await this.api.get(`/estimate-fee/${blocks}`).catch(this.handleError)
-    return response.data
+    // BTC/kB -> sat/B
+    const rate = ethers.utils.parseUnits(response.data.amount, 8).div(1000).toNumber()
+    return rate
   }
 
   async getTxHex(txId: string): Promise<string> {
-    const response = await this.api.get(`/tx/${txId}`).catch(this.handleError)
-    return response.data
+    const response = await this.api.get(`/tx?tx=${txId}`).catch(this.handleError)
+    return response.data.hex
   }
 
   async getOutputs(address: string): Promise<Utxo[]> {
-    const response = await this.api.post('/utxo', { addressList: [address] }).catch(this.handleError)
-    return response.data
+    const response = await this.api.post<UtxoResponse2WP>('/utxo', { addressList: [address] }).catch(this.handleError)
+    const { data: utxos } = response.data
+    return utxos.map(({ address, txid, vout, satoshis }) => ({
+      address,
+      txid,
+      amount: BigInt(satoshis),
+      vout,
+    }))
   }
 
   async getAddressDetails(address: string): Promise<AddressWithDetails> {
-    const response = await this.api.post('/address-info', { addressList: [address] }).catch(this.handleError)
-    return response.data
+    const response = await this.api.post('/addresses-info', { addressList: [address] }).catch(this.handleError)
+    const [details] = response.data.addressesInfo
+    return {
+      address: details.address,
+      balance: details.balance,
+      txCount: details.txs,
+    }
   }
 
   async broadcast(hexTx: string): Promise<string> {
     const response = await this.api.post('/broadcast', { data: hexTx }).catch(this.handleError)
-    return response.data
+    return response.data.txId
   }
 
   async getTransactionStatus<T extends TxType>(txHash: string, txType: T): Promise<Extract<StatusData, { type: T }>> {
