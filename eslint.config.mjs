d4a8d4d7bbe8a953e59c2b6dff9aec1ab177b5e6
lserra-iov <117093501+lserra-iov@users.noreply.github.com>
2025-09-26 16:11:37 -0300

    Peg-in fee estimation using 2WP API (#51)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   { plugins: { '@stylistic': stylistic } },
-  stylistic.configs['recommended-flat'],
+  stylistic.configs['recommended'],
   ...eslintPluginYml.configs['flat/standard'],
   {
     rules: {
@@ -21,6 +21,20 @@ export default [
       '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
       '@stylistic/arrow-parens': ['error', 'always'],
       'no-console': ['error'],
+      '@stylistic/lines-between-class-members': ['error', {
+        enforce: [
+          { blankLine: 'never', prev: 'field', next: 'field' },
+          { blankLine: 'always', prev: 'method', next: 'method' },
+          { blankLine: 'always', prev: 'field', next: 'method' },
+          { blankLine: 'always', prev: 'method', next: 'method' },
+        ],
+      }],
+      '@typescript-eslint/member-ordering': ['error', {
+        default: {
+          memberTypes: ['field', 'constructor', 'method'],
+          order: 'as-written',
+        },
+      }],
     },
   },
 ]
